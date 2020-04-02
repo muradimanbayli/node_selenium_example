02dad8ca937caa62dc3cd3f877e13475a138af4b
muradimanbayli <muradimanbayli@gmail.com>
2020-04-02 21:03:17 +0300

    download transfer csv

--- library.js
+++ library.js
@@ -1,9 +1,28 @@
-var webdriver = require('selenium-webdriver');
+const {Builder, By, until} = require('selenium-webdriver');
 
-var By = webdriver.By;
+(async function example() {
+    const driver = await new Builder().forBrowser('chrome').build();
 
-var until = webdriver.until;
+    try {
+        await driver.get('https://fleetsmarts.net/index.php/login');
+        await driver.executeScript("document.getElementById('username').setAttribute('value', 'garrett')");
+        await driver.executeScript("document.getElementById('password').setAttribute('value', 'retret13')");
+        
+        await driver.findElement(By.id('sign_in_button')).click();
 
-var driver = new webdriver.Builder().forBrowser('chrome').build();
+        await driver.findElement(By.xpath('/html/body/div[2]/div[1]/div[2]/div[2]/a[3]')).click();
+        
+        await driver.wait(until.elementLocated(By.id('org_filter')), 5 * 1000).then(el => {
+            driver.executeScript("document.getElementById('after_filter').setAttribute('value', '03/03/2020')");
+            driver.executeScript("document.getElementById('before_filter').setAttribute('value', '03/03/2020')"); 
+            driver.executeScript('document.querySelector("#org_filter > option:nth-child(92)").selected=true');
+            driver.executeScript('load_report();'); 
+        });
 
-driver.get('https://www.google.com');
+        driver.executeScript('download_report_transfer();');
+
+        await driver.wait(until.titleIs('nehalist.io'));
+    } finally {
+        //await driver.quit();
+    }
+})();
